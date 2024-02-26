4bbd20b4680a791a181600d425da1899112d9b66
Greatgabbar <strivedi_be19@thapar.edu>
2024-02-26 23:58:29 +0530

    changes done

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,13 @@
 FROM node:16
 
+EXPOSE 3000
+
 WORKDIR /usr/src/app
 
 COPY package* ./
 
 RUN npm install
+
+COPY . .
+
+CMD [ "npm","run","start" ]
--- index.js
+++ index.js
@@ -5,11 +5,23 @@ const plus = (x, y) => {
   return x + y;
 };
 
+const minus = (x, y) => {
+  return x - y;
+};
+
 app.get("/plus", (req, res) => {
   const { x, y } = req.query;
   if (!x || !y) return res.send("Add x and y as query parameters");
 
   const solution = plus(Number(x), Number(y));
+  res.send("Solution is " + solution);
+});
+
+app.get("/minus", (req, res) => {
+  const { x, y } = req.query;
+  if (!x || !y) return res.send("Add x and y as query parameters");
+
+  const solution = minus(Number(x), Number(y));
   res.send("Solution is " + solution);
 });
 
